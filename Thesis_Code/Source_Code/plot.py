b8467620589132bf2a6747353f16da9195f5e095
ElSarik <sarikakisilias@gmail.com>
2021-06-18 20:15:24 +0300

    Updated plot.py
    Function plot_history():
    + Edited comments
    
    -----------------------------------------
    
    Function draw_figure_w_toolbar():
    - Removed unused function on_key_press()
    + Added comments
    
    -----------------------------------------
    
    Class Toolbar():
    + Added comments

--- Thesis_Code/Source_Code/plot.py
+++ Thesis_Code/Source_Code/plot.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
+	#Forming the graph figure.
 	if canvas.children:
 		for child in canvas.winfo_children():
 			child.destroy()
@@ -18,19 +19,15 @@ def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
 	toolbar.update()
 	figure_canvas_agg.get_tk_widget().pack(side='right', fill='both', expand=1)
 
-	def on_key_press(event):
-		key_press_handler(event, canvas, toolbar)
-		canvas.TKCanvas.mpl_connect("key_press_event", on_key_press)
-	return
 	
 class Toolbar(NavigationToolbar2Tk):
-	# only display the buttons we need
+	#Adding the following tools to the toolbar.
 	toolitems = [t for t in NavigationToolbar2Tk.toolitems if
 				t[0] in ('Home', 'Pan', 'Zoom')]
-				# t[0] in ('Home', 'Pan', 'Zoom','Save')]
 
 	def __init__(self, *args, **kwargs):
 		super(Toolbar, self).__init__(*args, **kwargs)
+
 
 def plot_training_results(training_results):
 
@@ -49,17 +46,18 @@ def plot_training_results(training_results):
 	window = GUI.Window(title='Training Results', layout=layout)
 	window.Finalize()
 
-	#Loop for GUI window functions
+	#Loop for GUI window functions.
 	while True:
-		event, values = window.Read() #Reading events inside the GUI window
+		#Reading events inside the GUI window.
+		event, values = window.Read()
 
-		#Closing the window or pressing 'Continue' button breaks the loop
+		#Closing the window or pressing 'Continue' button breaks the loop.
 		if event == GUI.WIN_CLOSED or event =='Continue':
 			window.Close()
 			break
 
-		#Pressing 'Plot Graph' button plots the 'training_results' graph,
-		#together with the toolbar.
+		#Pressing 'Plot Graph' button generates the required axis to
+		#place the graph.
 		if event == 'Plot Graph':
 			fig, axs = plt.subplots(2)
 			fig.tight_layout(pad=4.0)
