e706d8e53169585ca08b2a5bb04c51fec2c70917
ElSarik <sarikakisilias@gmail.com>
2021-06-18 18:16:41 +0300

    Updated plot_history()
    Function plot_history():
    ! Renamed function to 'plot_training_results()'
    + Renamed function parameter 'history' to 'training_results'
    
    + Added comments
    + Added font variable
    + Restructured layout
      -> Added font to Text and Buttons
      -> Added pad to Buttons
      -> Renamed parameters 'GUI.T' to 'GUI.Text' & 'GUI.B' to 'GUI.Button'
      -> Renamed 'Plot' button to 'Plot Graph'
      -> Renamed window to 'Training Results'

--- Thesis_Code/Source_Code/plot.py
+++ Thesis_Code/Source_Code/plot.py
@@ -32,45 +32,47 @@ class Toolbar(NavigationToolbar2Tk):
 	def __init__(self, *args, **kwargs):
 		super(Toolbar, self).__init__(*args, **kwargs)
 
-def plot_history(history):
+def plot_training_results(training_results):
+
+	font = ('Arial', 13) #Font for the layout.
+
+	#Forming the layout for the 'Training Results' GUI window.
 	layout = [
-		# [GUI.T('Graph: y=sin(x)')],
-		[GUI.B('Plot'), GUI.B('Continue')],
-		[GUI.T('Controls:')],
-		[GUI.Canvas(key='controls_cv')],
-		[GUI.T('Figure:')],
-		[GUI.Column(
-			layout=[
-				[GUI.Canvas(key='fig_cv',
-							# it's important that you set this size
-							size=(400 * 2, 400)
-							)]
-			],
-			background_color='#DAE0E6',
-			pad=(0, 0)
-		)],
-		]
-	window = GUI.Window(title='Graph with controls', layout=layout)
+			  [GUI.Text('Controls:', font=font)],
+			  [GUI.Canvas(key='controls_cv')],
+			  [GUI.Text('Figure:', font=font)],
+			  [GUI.Column(layout=[[GUI.Canvas(key='fig_cv',size=(400 * 2, 450))]], background_color='#DAE0E6', pad=(0, 0))],
+			  [GUI.Button('Plot Graph', font=font, pad=(155,5)), GUI.Button('Continue', font=font, pad=(155,5))],
+			 ]
+
+	#Finalizing GUI window creation.
+	window = GUI.Window(title='Training Results', layout=layout)
 	window.Finalize()
 
+	#Loop for GUI window functions
 	while True:
-		event, values = window.Read()
-		if event == GUI.WIN_CLOSED or event =='Continue':  # always,  always give a way out!
+		event, values = window.Read() #Reading events inside the GUI window
+
+		#Closing the window or pressing 'Continue' button breaks the loop
+		if event == GUI.WIN_CLOSED or event =='Continue':
 			window.Close()
 			break
-		if event == 'Plot':
+
+		#Pressing 'Plot Graph' button plots the 'training_results' graph,
+		#together with the toolbar.
+		if event == 'Plot Graph':
 			fig, axs = plt.subplots(2)
 			fig.tight_layout(pad=4.0)
 
-			axs[0].plot(history.history["accuracy"], label="train accuracy")
-			axs[0].plot(history.history["val_accuracy"], label="validation accuracy")
+			axs[0].plot(training_results.history["accuracy"], label="train accuracy")
+			axs[0].plot(training_results.history["val_accuracy"], label="validation accuracy")
 			axs[0].set_ylabel("Accuracy")
 			axs[0].set_xlabel("Epoch")
 			axs[0].legend(loc="lower right")
 			axs[0].set_title("Accuracy Evaluation")
 
-			axs[1].plot(history.history["loss"], label="train error")
-			axs[1].plot(history.history["val_loss"], label="validation error")
+			axs[1].plot(training_results.history["loss"], label="train error")
+			axs[1].plot(training_results.history["val_loss"], label="validation error")
 			axs[1].set_ylabel("Error")
 			axs[1].set_xlabel("Epoch")
 			axs[1].legend(loc="upper right")
